a24131167f7833371d653e7ea87daf13eb647f32
dl-eric <orangepamelo@gmail.com>
2015-10-02 16:02:59 -0400

    Add Modal

--- js/profile.js
+++ js/profile.js
@@ -8,13 +8,27 @@ $(document).ready(function () {
 
                 //For Guest
                 $("#account a").prepend("Guest <b class=\"caret\"></b>");
-                $("#account >.dropdown-menu").append("<li><a href=\"/login\">Login</a></li>");
-                $("#account >.dropdown-menu").append("<li><a href=\"/register\">Register</a></li>");
+                $("#account >.dropdown-menu").append("<li><a href=\"#\" id=\"loginLink\">Login\/Register</a></li>");
             } else {
                 $("#account a").prepend(email + " <b class=\"caret\"></b>");
             }
 
             //Display username after scripts finish
             $("#account").show();
+            $("#loginLink").click(function (event) {
+                event.preventDefault();
+                $(".overlay").fadeToggle("fast");
+            });
+
+            $(".close").click(function () {
+                $(".overlay").fadeToggle("fast");
+            });
+
+            $(document).keyup(function (e) {
+                if (e.keyCode == 27 && $(".overlay").css("display") != "none") {
+                    event.preventDefault();
+                    $(".overlay").fadeToggle("fast");
+                }
+            });
         }, 1);
 });
