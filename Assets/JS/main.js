b84deb18a2b0f20e9e46e4db577f5f69d244ee37
Jeffrey Shen <jeffreyshencc@gmail.com>
2016-05-23 22:26:50 -0400

    Added tech guide support (WIP), better mobile support, improved members page (club roster and work duty)

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,3 +2,31 @@ function toggleChat() {
     $_Tawk.toggle();
     return false;
 }
+
+function dropdown(){
+    $("#dropdown").toggle();
+}
+
+$( document ).ready(function() {
+    if($(window).width() < 750){
+        //Create dropdown
+        $("nav .right a").hide();
+        $("#menu").show();
+        $("#dropdown").hide();
+    }else{
+        //$("#dropdown").hide();
+        //$("#menu").hide();
+    }
+});
+
+$(window).resize(function() {
+    if($(window).width() < 750){
+        //Create dropdown
+        $("nav .right a").hide();
+        $("#menu").show();
+    }else{
+        $("nav .right a").show();
+        $("#menu").hide();
+        $("#dropdown").hide();
+    }
+});
